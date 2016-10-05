ba7e95127e0946a7d025e395ddfc16d600f05c50
Ronaldo Lima <2dlima@2d-Lima.local>
2016-10-04 23:43:43 -0300

    clear house

--- campaigns/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router  = express.Router();
-
-// localhost:3001/api/campaigns/:IDdoJob/ pieces where job:id
-router.get('/:id', require('./services/find'));
-
-module.exports = router;
