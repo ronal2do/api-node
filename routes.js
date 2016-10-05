921f1111920e5a8925889bc5146725f781156306
Ronaldo Lima <2dlima@2d-Lima.local>
2016-10-05 00:23:31 -0300

    routes

--- routes.js
+++ routes.js
@@ -1,8 +1,7 @@
 module.exports = function (app) {
     app.use('/api/pieces', require('./pieces'));
     app.use('/api/jobs', require('./jobs'));
-    app.use('/api/campaigns', require('./campaigns'));
-    
+
     var contactController = require('./contact/services/create');
     app.post('/api/contact', contactController.contactPost);
 };
